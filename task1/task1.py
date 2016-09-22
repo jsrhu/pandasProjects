607fc6f66f7e2d15f4f581786dc90c2b7c7dfc38
Joshua Hu <jsrhu@Joshuas-MacBook-Pro.local>
2016-09-22 15:46:08 -0400

    read and print compressed accern data from as dataframe object

--- task1/task1.py
+++ task1/task1.py
@@ -5,4 +5,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-with gzip.open()
+compressed_data_2014 = '../data/2014all_head.csv.gz'
+compressed_data_2015 = '../data/2015all_head.csv.gz'
+compressed_data_2016 = '../data/2016all_head.csv.gz'
+
+dataframe_2014 = pd.read_csv(compressed_data_2014,chunksize=4)
+
+for chunk in dataframe_2014:
+    print chunk
