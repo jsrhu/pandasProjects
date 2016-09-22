d43d5dc6b8f49e8fba9d92c9b474de9e6fd469b0
Joshua Hu <jsrhu@Joshuas-MacBook-Pro.local>
2016-09-22 14:46:48 -0400

    added .gitignore

--- pandasIntro.py
+++
@@ -1,5 +0,0 @@
-# following introduction from documentation in http://pandas.pydata.org/pandas-docs/version/0.18.1/10min.html as of 17/08/16
-# reinterpreted as file as opposed to notebook environment (Jupyter)
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
