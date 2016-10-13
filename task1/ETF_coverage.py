babea87d3fb7964a15a2863ce5e5c5d9a90f63d7
Joshua Hu <jsrhu@HAL-9002.local>
2016-10-12 23:12:07 -0400

    update

---
+++ task1/ETF_coverage.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 10:29:48 2016
+
+@author: jsrhu
+"""
+
+import sys
+import os
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import readData
+from stringProcess import removePunctuation
+
+'''
+implement scraper for
+SPDR:
+    SPY
+    MDY
+Blackrock:
+    IWM
+'''
+
+'''
+create function for general ETF
+'''
+
+compressed_data = '../data/csv/backtest_alpha_for_ernest.csv.gz'
+
+etf_path = '../data/etf/'
+
+data_SPY = etf_path+'SPY_All_Holdings.xls'
+data_MDY = etf_path+'MDY_All_Holdings.xls'
+data_IWM = etf_path+'IWM_holdings.csv'
+
+df_SPY = readData.readToDF(data_SPY)
+df_MDY = readData.readToDF(data_MDY)
+df_IWM = readData.readToDF(data_IWM)
+
+df_SPY.dropna(inplace=True)
+df_MDY.dropna(inplace=True)
+df_IWM.dropna(inplace=True)
+
+df_SPY = df_SPY.rename(columns={'Identifier':'Ticker'})
+df_MDY = df_MDY.rename(columns={'Identifier':'Ticker'})
+
+df_SPY['Ticker'] = df_SPY['Ticker'].apply(removePunctuation)
+df_MDY['Ticker'] = df_MDY['Ticker'].apply(removePunctuation)
+df_IWM['Ticker'] = df_IWM['Ticker'].apply(removePunctuation)
+
+count_tickers_SPY = df_SPY['Ticker'].size
+count_tickers_MDY = df_MDY['Ticker'].size
+count_tickers_IWM = df_IWM['Ticker'].size
+
+date_format="%Y-%m-%d %H:%M:%S %Z"
+dateparse = lambda x: pd.datetime.strptime(x, date_format)
+rows = 100000
+#df_accern = pd.read_csv(filepath_or_buffer=compressed_data, compression='gzip', nrows=rows, header=0, date_parser=dateparse, parse_dates=["harvested_at"])
+df_accern = pd.read_csv(filepath_or_buffer=compressed_data, compression='gzip', header=0, date_parser=dateparse, parse_dates=["harvested_at"])
+dfgroup_accern = df_accern.groupby( df_accern['harvested_at'].apply(lambda x: x.date() ) )
+
+columns = ['SPY_coverage','MDY_coverage','IWM_coverage']
+
+df_ETF_coverage_average_day = pd.DataFrame(index=dfgroup_accern.indices, columns=columns)
+
+sr_SPY = pd.Series()
+sr_MDY = pd.Series()
+sr_IWM = pd.Series()
+
+for date,group in dfgroup_accern:
+    tickers = group['entities_ticker_1'].unique()
+    count_SPY,count_MDY,count_IWM = 0,0,0
+    for accern_ticker in tickers:
+        if accern_ticker in df_SPY['Ticker'].values:
+            count_SPY=count_SPY+1
+        if accern_ticker in df_MDY['Ticker'].values:
+            count_MDY=count_MDY+1
+        if accern_ticker in df_IWM['Ticker'].values:
+            count_IWM=count_IWM+1
+    coverage_SPY = count_SPY/float(count_tickers_SPY-1)*100
+    coverage_MDY = count_MDY/float(count_tickers_MDY-1)*100
+    coverage_IWM = count_IWM/float(count_tickers_IWM)*100
+
+    sr_SPY[date] = coverage_SPY
+    sr_MDY[date] = coverage_MDY
+    sr_IWM[date] = coverage_IWM
+    
+df_ETF_coverage_average_day['SPY_coverage'] = sr_SPY
+df_ETF_coverage_average_day['MDY_coverage'] = sr_MDY
+df_ETF_coverage_average_day['IWM_coverage'] = sr_IWM
+
+try:
+    output = os.mkdir('output')
+except:
+    output = 'output'
+os.chdir(output)
+
+ax = df_ETF_coverage_average_day.plot(title='ETF Coverage Over Time by Accern Data',rot=45, figsize=(11,8))
+ax.set_ylabel('Percentage of ETF')
+ax.set_xlabel('Date')
+lgd = plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+plt.tight_layout()
+plt.savefig('ETF_coverage'+str(rows)+'rows.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+plt.clf()
--- task1/task1.py
+++ task1/task1.py
@@ -8,6 +8,7 @@ STATUS:
 TODO
 - change path to output to ignored directory
 """
+import sys
 import os
 import pandas as pd
 
@@ -18,9 +19,9 @@ from pptx.util import Inches
 
 from urlparse import urlparse
 
-import readWrite
+# import readWrite
 
-compressed_data = '../data/backtest_alpha_for_ernest.csv.gz'
+compressed_data = '../data/csv/backtest_alpha_for_ernest.csv.gz'
 
 headers = ['story_sentiment','story_volume','story_traffic','story_shares','article_sentiment','article_traffic','event_impact_score_overall','event_impact_score_entity_1','event_impact_score_entity_2','avg_day_sentiment']
 
@@ -52,7 +53,7 @@ Does not return anything
 def readFull(csv_file):
     date_format="%Y-%m-%d %H:%M:%S %Z"
     dateparse = lambda x: pd.datetime.strptime(x, date_format)
-    pd.read_csv(filepath_or_buffer=csv_file, compression='gzip', header=0, date_parser=dateparse, parse_dates=["harvested_at"])
+    return pd.read_csv(filepath_or_buffer=csv_file, compression='gzip', header=0, date_parser=dateparse, parse_dates=["harvested_at"])
 
 '''
 Returns dataframe with select specififed columns from the original
@@ -64,8 +65,8 @@ def selectColumnsDataFrame(data_frame,columns):
 '''
 increases the y scale of a pyplot instance by a given decimal percentage
 '''
-def increaseYScaleRange(plot,percentage):
-    plot.ylim(df_min[column].min()-abs(percentage*df_min[column].min()),df_max[column].max()+abs(percentage*df_max[column].max()))
+def increaseYScaleRange(plot, dataframe_min, dataframe_max, column, percentage):
+    plot.ylim(dataframe_min[column].min()-abs(percentage*dataframe_min[column].min()),dataframe_max[column].max()+abs(percentage*dataframe_max[column].max()))
 
 '''
 Attempts to create directory for plot images
@@ -81,94 +82,153 @@ def createDir(name, descriptor):
 
 def addImageSlide(presentation, layout, image_path):
     slide_layout = presentation.slide_layouts[layout]
-    
+
     slide = presentation.slides.add_slide(slide_layout)
-    
+
     left = Inches(1)
     top = Inches(1)
     height = Inches(5.5)
     return slide.shapes.add_picture(image_file=image_path, left=left, top=top, height=height)
-    
+
 def savePresentation(presentation, title):
     presentation.save(title+'.pptx')
-    
-df = readLimitedRows(csv_file=compressed_data,rows=10000)
-df_max = pd.DataFrame()
-df_min = pd.DataFrame()
-    
-df_group = df.groupby( df['harvested_at'].apply(lambda x: x.date() ) )
 
-'''
-form as one dataframe; pickle the file
-'''
+def main():
+    rows=100000
+    #df = readLimitedRows(csv_file=compressed_data,rows=rows)
+    df = readFull(csv_file=compressed_data)
+    last_date = df['harvested_at'].max().date()
+    df_max = pd.DataFrame()
+    df_min = pd.DataFrame()
+    df_group = df.groupby( df['harvested_at'].apply(lambda x: x.date() ) )
+    try:
+        output = os.mkdir('output')
+    except:
+        output = 'output'
+    os.chdir(output)
 
-ticker_count_1 = df_group['entities_ticker_1'].nunique()
-ticker_count_1.plot(title='Number of Primary Tickers from Harvested Articles by Day', rot=45)
-plt.savefig('unique_ticker1_plot.png')
-plt.clf()
+    '''
+    ticker_count_1 = df_group['entities_ticker_1'].nunique()
+    ticker_count_1.plot(title='Number of Primary Tickers from Harvested Articles by Day', rot=45, x_compat=True)
+    plt.tight_layout()
+    plt.savefig('unique_ticker1_plot_'+str(rows)+'rows.png')
+    plt.clf()
 
-'''
-FOR LATER USE
-ticker1 = df_group['entities_ticker_1'].unique()
-for date in ticker1:
-    for ticker in date:
-        ticker.translate(string.maketrans("",""),string.punctuation)
+    ticker_count_2 = df_group['entities_ticker_2'].nunique()
+    ticker_count_2.plot(title='Number of Secondary Tickers from Harvested Articles by Day', rot=45, x_compat=True)
+    plt.tight_layout()
+    plt.savefig('unique_ticker2_plot_'+str(rows)+'rows.png')
+    plt.clf()
+    '''
 
-ticker1.to_csv(path='ticker1.txt')
-'''
-
-ticker_count_2 = df_group['entities_ticker_2'].nunique()
-ticker_count_2.plot(title='Number of Secondary Tickers from Harvested Articles by Day', rot=45)
-plt.savefig('unique_ticker2_plot.png')
-plt.clf()
-
-url_list = []
-url_count = df_group['article_url'].nunique()
-for row in df_group['article_url'].unique():
-    for url in row:
-        url_list.append(urlparse(url).netloc)
-df_url = pd.Series(url_list, dtype='string')
-df_url.sort_values(inplace=True)
-df_url.drop_duplicates(inplace=True)
-print df_url.nunique()
-df_url.to_csv(path='unique_urls.txt')
-
-for key,group in df_group:
-    group_max = group.max(numeric_only=True)
-    group_min = group.min(numeric_only=True)
-    group_max['date'] = key
-    group_min['date'] = key
+    df_avg_day = pd.DataFrame(columns=['dofm','total','day_avg'])
+    sr_dofm = pd.Series()
+    list_url = []
+    for date,row in df_group['article_url'].unique().iteritems():
+        for url in row:
+            domain = urlparse(url).netloc
+            list_url.append(domain)
+            
+            if domain in sr_dofm:
+                pass
+            else:
+                sr_dofm[domain] = date
+    sr_url = pd.Series(list_url)
+    sr_value_count = sr_url.value_counts(ascending=True)
     
-    df_max = pd.concat([df_max, group_max.to_frame().T])
-    df_min = pd.concat([df_min, group_min.to_frame().T])
-    
-df_max.set_index(keys='date', inplace=True)
-df_min.set_index(keys='date', inplace=True)
-    
-df_max = selectColumnsDataFrame(data_frame=df_max,columns=headers)
-df_min = selectColumnsDataFrame(data_frame=df_min,columns=headers)
-    
-plot_dir = createDir(name=plot_type, descriptor='plots')
-os.chdir(plot_dir)
+    df_avg_day['dofm'] = sr_dofm    
+    df_avg_day['total'] = sr_value_count
 
-presentation = Presentation()
-descriptor = 'max_min_plot'
-title_slide = presentation.slides.add_slide(presentation.slide_layouts[layout_title])
-title_slide.shapes.title.text = 'Maximum and Minimum Plots of Quantitative Metrics in:\n'+'Accern Data'
+    df_avg_day['day_avg'] = df_avg_day['total'].div(df_avg_day['dofm'].rsub(last_date).dt.days+1)
+    df_avg_day = df_avg_day.sort_values(by='day_avg')
+
+    print df_avg_day.head(50)
+    print df_avg_day.tail(50)
     
-for column in df_max:
-    column_plot = df_max[column].plot()
-    column_plot = df_min[column].plot(ax=column_plot, title=column, x_compat=True, rot=45, figsize=(11,8))
-        
-    increaseYScaleRange(plt, .10)
+    print df_avg_day['day_avg'].quantile([.1,.5,.99])
     
-    image_path = column+'_'+descriptor+'.png'
-    plt.savefig(image_path)
+    try:
+        print 'Average daily reference to "www.benzinga.com":',df_avg_day['day_avg']['www.benzinga.com']
+    except:
+        print 'The domain: "www.benzinga.com" could not be found.'
+    try:
+        print 'Average daily reference to "www.bloomberg.com":',df_avg_day['day_avg']['www.bloomberg.com']
+    except:
+        print 'The domain: "www.bloomberg.com" could not be found.'
+    try:
+        print 'Average daily reference to "www.reuters.com":',df_avg_day['day_avg']['www.reuters.com']
+    except:
+        print 'The domain: "www.reuters.com" could not be found.'
+    try:
+        print 'Average daily reference to "finance.yahoo.com":',df_avg_day['day_avg']['finance.yahoo.com']
+    except:
+        print 'The domain: "finance.yahoo.com" could not be found.'
+    try:
+        print 'Average daily reference to "www.wsj.com":',df_avg_day['day_avg']['www.wsj.com']
+    except:
+        print 'The domain: "www.wsj.com" could not be found.'
+
+    bin_range = [x+20 for x in xrange(int(df_avg_day['total'].max())+1)]
+    ax = sr_value_count.plot.hist(logy=False, bins=bin_range)
+    ax.set_ylabel('Number of Domains')
+    ax.set_xlabel('Number of Total References')
+    plt.tight_layout()
+    plt.savefig('domain_total.png')
     plt.clf()
     
-    addImageSlide(presentation=presentation, layout=layout_blank, image_path=image_path)
+    bin_range = [x+20 for x in xrange(int(df_avg_day['day_avg'].max())+1)]
+    ax = df_avg_day['day_avg'].plot.hist(logy=False, bins=bin_range)
+    ax.set_ylabel('Number of Domains')
+    ax.set_xlabel('Number of Average References')
+    plt.tight_layout()
+    plt.savefig('domain_day_avg.png')
+    plt.clf()
 
-os.chdir('..')
-ppt_dir = createDir(name='ppt', descriptor=descriptor)
-os.chdir(ppt_dir)
-savePresentation(presentation,descriptor)
+    '''
+    should look for a better way to do this
+    perhaps new df called df_minmax_$group where $group is one of the numeric columns
+    for key,group in df_group:
+        group_max = group.max(numeric_only=True)
+        group_min = group.min(numeric_only=True)
+        group_max['date'] = key
+        group_min['date'] = key
+
+        df_max = pd.concat([df_max, group_max.to_frame().T])
+        df_min = pd.concat([df_min, group_min.to_frame().T])
+
+    df_max.set_index(keys='date', inplace=True)
+    df_min.set_index(keys='date', inplace=True)
+
+    df_max = selectColumnsDataFrame(data_frame=df_max,columns=headers)
+    df_min = selectColumnsDataFrame(data_frame=df_min,columns=headers)
+
+    plot_dir = createDir(name=plot_type, descriptor='plots')
+    os.chdir(plot_dir)
+
+    presentation = Presentation()
+    descriptor = 'max_min_plot'
+    title_slide = presentation.slides.add_slide(presentation.slide_layouts[layout_title])
+    title_slide.shapes.title.text = 'Maximum and Minimum Plots of Quantitative Metrics in:\n'+'Accern Data'
+
+    for column in df_max:
+        column_plot = df_max[column].plot()
+        column_plot = df_min[column].plot(ax=column_plot, title=column, x_compat=True, rot=45, figsize=(11,8))
+
+        increaseYScaleRange(plt, dataframe_min=df_min, dataframe_max=df_max, column=column, percentage=.10)
+
+        image_path = column+'_'+descriptor+'.png'
+        plt.tight_layout()
+        plt.savefig(image_path)
+        plt.clf()
+
+        addImageSlide(presentation=presentation, layout=layout_blank, image_path=image_path)
+
+    os.chdir('..')
+    ppt_dir = createDir(name='ppt', descriptor=descriptor)
+    os.chdir(ppt_dir)
+    savePresentation(presentation,descriptor)
+    '''
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
